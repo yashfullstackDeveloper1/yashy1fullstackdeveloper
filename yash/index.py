43fcaf32f13f556eb6f7cd6df7389403c97dc6c0
yashfullstackDeveloper1 <yashdhapke6@gmail.com>
2025-11-12 21:51:29 +0530

    Add files via upload

---
+++ yash/index.py
@@ -0,0 +1,8 @@
+while(True):
+    choice = str(input("enter a choice"))
+    if(choice == "poweron"):
+        print("car is started")
+        isOn = True
+
+        else:
+            print("car is stopped")
